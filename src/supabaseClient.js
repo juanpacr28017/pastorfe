48b6a1a503b16989dc4971861b309d7f83283b7a
juanpacr28017 <73553115+juanpacr28017@users.noreply.github.com>
2025-10-24 13:20:07 +0200

    Update supabaseClient.js con log

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,5 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+console.log("Supabase URL:", supabaseUrl);
+console.log("Supabase Anon Key:", supabaseAnonKey?.slice(0,10) + "..."); // no mostramos toda la key
 
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
